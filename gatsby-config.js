d80dd211f5d430d9e1b606c6238aeaf16b510148
Paul <paul@theapplegates.com>
2020-01-12 19:26:15 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Starter Blog`,
     author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://gat-start-jan10.netlify.com/`,
     social: {
       twitter: `kylemathews`,
     },
@@ -30,6 +30,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
+              withWebp: true,
               maxWidth: 590,
             },
           },
